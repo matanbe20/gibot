bb8fcb261eb226a6ba2675b5f00a2e2b944116f4
Matan <matanbe18@gmail.com>
2019-05-29 00:16:17 +0300

    path chnage

--- bot.js
+++ bot.js
@@ -19,8 +19,8 @@ client.on('message', message => {
       .join()
       .then(connection => {
         try {
-          const dispatcher = connection.playFile('bell.mp3');
-          console.log('playing song : song.mp3')
+          const dispatcher = connection.playFile('./bell.mp3');
+          console.log('playing song : bell.mp3')
           dispatcher.on('end', ()=> {
             message.member.voiceChannel.leave()
           }) 
