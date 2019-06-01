9e504b3271ee92178b062a2301b68ed71a0a04a1
Matan <matanbe18@gmail.com>
2019-06-01 09:04:25 +0300

    changed path

--- bot.js
+++ bot.js
@@ -1,17 +1,21 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 var path = require('path');
-console.log('index file loaded')
-const TOKEN = process.env.BOT_TOKEN;
+console.log('index file loaded');
+const TOKEN =
+  process.env.BOT_TOKEN ||
+  'NTgxMTgyOTYzNDU1MzYxMDQ1.XOeLuQ.o_jy4GGxO5NB1DOFZoKIAUhAQlg';
 client.login(TOKEN);
 
-client.on('ready', ()=> {
+client.on('ready', () => {
   console.log('Bot is up and running ... yay');
-  
-})
+});
 
 client.on('message', message => {
   // Voice only works in guilds, if the message does not come from a guild,
+  const {content} = message;
+
+  if(content !== 'now') return;
 
   // Only try to join the sender's voice channel if they are in one themselves
   if (message.member.voiceChannel) {
@@ -19,13 +23,15 @@ client.on('message', message => {
       .join()
       .then(connection => {
         try {
-          const dispatcher = connection.playFile(path.resolve(__dirname, 'bell.mp3'));
-          console.log('playing song : bell.mp3')
-          dispatcher.on('end', ()=> {
-            message.member.voiceChannel.leave()
-          }) 
-        } catch(e) {
-          console.log(e)  
+          const dispatcher = connection.playFile(
+            path.resolve(__dirname, 'audio',  'bell.mp3')
+          );
+          console.log('playing song : bell.mp3');
+          dispatcher.on('end', () => {
+            message.member.voiceChannel.leave();
+          });
+        } catch (e) {
+          console.log(e);
         }
       })
       .catch(console.log);
