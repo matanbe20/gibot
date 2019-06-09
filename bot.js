24f06ccc3f3683bb6005242716570bcc1f371597
Matan Ben Moshe <matanbe18@gmail.com>
2019-06-09 19:19:29 +0300

    שגגקג ךקשהק

--- bot.js
+++ bot.js
@@ -11,13 +11,15 @@ client.on("ready", () => {
 
 let isPlaying = false;
 
-const randomizeResult = array => {
+const randomizeResult = (array, randomizerValue) => {
   let numberRandom;
-  if (array.length > 12) numberRandom = 12;
+  if (array.length > randomizerValue) numberRandom = randomizerValue;
   else numberRandom = array.length;
   const randomNum = Math.floor(Math.random() * numberRandom) + 0;
   return array[randomNum];
 };
+
+let entity = "musicArtist";
 
 client.on("message", async message => {
   // Only try to join the sender's voice channel if they are in one themselves
@@ -25,28 +27,37 @@ client.on("message", async message => {
   const content = message.content;
   let term = "";
   let songMetadata = {};
-
+  let isSong = false;
   if (content.indexOf("!g") === 0) {
     term = content.split("!g ")[1];
+    if (term === "song") {
+      isSong = true;
+      entity = "song";
+      term = content.split("!g song ")[1];
+    }
     term = term.replace(" ", "+");
-    console.log(`https://itunes.apple.com/search?term=${term}`);
   } else return;
 
   if (message.member.voiceChannel) {
     const response = await fetch(
-      `https://itunes.apple.com/search?term=${term}`
+      `https://itunes.apple.com/search?term=${term}&media=music`
     );
     const data = await response.json();
     if (!data || !data.results.length)
       return message.reply("No song was found :(");
-    songMetadata = randomizeResult(data.results);
+    songMetadata = isSong
+      ? randomizeResult(data.results, 2)
+      : randomizeResult(data.results, 15);
 
     const channel = message.member.voiceChannel;
     const connection = await channel.join();
     const dispatcher = connection.playStream(songMetadata.previewUrl);
-    await message.reply(
-      `Playing: "${songMetadata.trackName}"  By: ${songMetadata.artistName}`
-    );
+    const log = `Playing: "${songMetadata.trackName}"  By ${
+      songMetadata.artistName
+    }`;
+    console.log(log);
+    await message.reply(log);
+    isPlaying = true;
     message.channel.send(songMetadata.artworkUrl100);
 
     dispatcher.on("end", () => {
